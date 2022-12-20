d26133af14fa73b03fe03a1a6ae9f9b80898cf91
alreadytakent <117181738+alreadytakent@users.noreply.github.com>
2022-12-20 16:25:40 +0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 
 engine_team = 1
 b = (-1)**engine_team
@@ -8,7 +8,7 @@ piece_values = {'p': 100*b, 'n': 300*b, 'b': 300*b, 'r': 500*b, 'q': 900*b, 'k':
 
 def engine_move(Board):
     possible_moves = [move for move in Board.legal_moves()]
-    return random.choice(possible_moves)
+    return choice(possible_moves)
 
 def evaluate(Board):
     E = 0
